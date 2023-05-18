6cab4e6de2a4426b1c25c514ec338d0219900eed
Ryan Sjostrand <sjostrand@hpe.com>
2023-05-18 14:25:02 -0500

    Checkin

--- internal/provider/csm/reconcile.go
+++ internal/provider/csm/reconcile.go
@@ -1,8 +1,7 @@
 package csm
 
-import "fmt"
-
 // Reconcile CANI's inventory state with the external inventory state and apply required changes
 func (csm *CSM) Reconcile() error {
-	return fmt.Errorf("todo")
+
+	return nil
 }
