9e3f22e87ea3334d79cc51415c54fea07d43cdfd
Jacob Salmela <jacob.salmela@hpe.com>
2023-06-15 10:53:59 -0500

    demo
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

---
+++ demo.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Run from ncn-m002!
+
+if [[ $DEBUG == "true" ]]; then
+  set -x
+fi
+
+# show auth using system certs
+rm -rf ~/.cani && bin/cani alpha session start csm \
+  --csm-keycloak-username vshasta \
+  --csm-keycloak-password vshasta \
+  --csm-base-auth-url https://api-gw-service-nmn.local/ \
+  --csm-url-sls https://api-gw-service-nmn.local/apis/sls/v1
+
+# show only one system in the inventory added by default (also show the basic inventory structure)
+bin/cani alpha list | jq
+
+# add a new cabinet
+bin/cani alpha add cabinet
+bin/cani alpha add cabinet hpe-ex2000
+# show various errors
+bin/cani alpha add cabinet hpe-ex2000 --cabinet 1
+bin/cani alpha add cabinet hpe-ex2000 --vlan-id 1
+# show successful add
+bin/cani alpha add cabinet hpe-ex2000 --vlan-id 1 --cabinet 1
+bin/cani alpha list cabinet | jq
+# optionally, show auto-added child hardware
+# bin/cani alpha list | jq
+
+# try to add the same cabinet number should fail
+bin/cani alpha add cabinet hpe-ex2000 --vlan-id 1 --cabinet 1
+# same for the same vlan-id (this does not work currently since we aren't checking vlan uniqueness)
+# bin/cani alpha add cabinet hpe-ex2000 --vlan-id 1 --cabinet 2
+
+# add blade in the in same way
+bin/cani alpha add blade hpe-crayex-ex420-compute-blade --cabinet 1 --chassis 1 --blade 0
+bin/cani alpha update node --cabinet 1 --chassis 1 --blade 0 --nodecard 1 --node 0 --role "Compute" --alias "nid00001" --nid 1
---
+++ setup_demo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# mkdir -p cani_demo && cd !$
+# git clone https://github.com/Cray-HPE/cani.git 
+# git checkout supreme-sacrifice
+
+pushd ~/cani_demo/cani || exit 1
+  # ensure go is in the path for this script
+  export PATH=$PATH:/usr/local/go/bin
+  if ! command -v go; then
+    # get and install go
+    wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+  fi
+  
+  # modify SLS to pass checks
+  cray sls dumpstate list --format json > sls_dump_"$(date +"%s")".json # Backup just in case :)
+  cray sls networks update ./hmn_mtn.json HMN_MTN
+  cray sls networks update ./nmn_mtn.json NMN_MTN
+
+  # Make the binary
+  make bin
+
+popd || exit 1
+
