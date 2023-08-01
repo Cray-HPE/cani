c836d2763a0da014c3591e9644e602223316ae87
Shane Unruh <shane.unruh@hpe.com>
2023-08-01 12:40:24 -0600

    Added type comparison to location path comparisons

--- internal/inventory/model_util_test.go
+++ internal/inventory/model_util_test.go
@@ -26,6 +26,9 @@
 package inventory
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/Cray-HPE/cani/pkg/hardwaretypes"
@@ -37,6 +40,26 @@ func buildLocation(ordinals ...int) LocationPath {
 		loc = append(loc, LocationToken{Ordinal: ordinal})
 	}
 	return loc
+}
+
+func toLocationPath(location string) (LocationPath, error) {
+	path := LocationPath{}
+	tokens := strings.Split(location, "->")
+	for _, t := range tokens {
+		parts := strings.SplitN(t, ":", 2)
+		if len(parts) != 2 {
+			return path, fmt.Errorf("location path string invalid. path: %v, part: %v", location, t)
+		}
+		tt := hardwaretypes.HardwareType(parts[0])
+		ordinal, err := strconv.ParseInt(parts[1], 10, 0)
+		if err != nil {
+			return path, err
+		}
+		token := LocationToken{HardwareType: tt, Ordinal: int(ordinal)}
+
+		path = append(path, token)
+	}
+	return path, nil
 }
 
 func TestCompareHardwareByTypeThenLocation(t *testing.T) {
@@ -56,7 +79,7 @@ func TestCompareHardwareByTypeThenLocation(t *testing.T) {
 
 	v = CompareHardwareByTypeThenLocation(&hw2, &hw1)
 	if v {
-		t.Fatalf("Expected false when comparing Node to Cabinet, hw1: %v, hw2: %v", hw2, hw1)
+		t.Fatalf("Expected false when comparing Node to Cabinet, hw2: %v, hw1: %v", hw2, hw1)
 	}
 
 	hw3 := Hardware{Type: hardwaretypes.Node}
@@ -64,18 +87,54 @@ func TestCompareHardwareByTypeThenLocation(t *testing.T) {
 	hw3.LocationPath = buildLocation(1, 2, 3)
 	v = CompareHardwareByTypeThenLocation(&hw2, &hw3)
 	if v {
-		t.Fatalf("Expected false when comparing same hardware with locations, hw1: %v, hw2: %v", hw2, hw3)
+		t.Fatalf("Expected false when comparing same hardware with locations, hw2: %v, hw3: %v", hw2, hw3)
 	}
 
 	hw2.LocationPath = buildLocation(1, 2, 3)
 	hw3.LocationPath = buildLocation(1, 2, 4)
 	v = CompareHardwareByTypeThenLocation(&hw2, &hw3)
 	if !v {
-		t.Fatalf("Expected true when comparing hardware with locations, hw1: %v, hw2: %v", hw2, hw3)
+		t.Fatalf("Expected true when comparing hardware with locations, hw2: %v, hw3: %v", hw2, hw3)
 	}
 
 	v = CompareHardwareByTypeThenLocation(&hw3, &hw2)
 	if v {
-		t.Fatalf("Expected false when comparing hardware with locations, hw1: %v, hw2: %v", hw3, hw2)
+		t.Fatalf("Expected false when comparing hardware with locations, hw3: %v, hw2: %v", hw3, hw2)
+	}
+}
+
+func TestCompareLocationPath(t *testing.T) {
+	location0, err := toLocationPath("System:0->Cabinet:1003->Chassis:7->NodeBlade:7->NodeCard:1->Node:0")
+	if err != nil {
+		t.Fatalf("error parsing location0, err: %v", err)
+	}
+	location1, err := toLocationPath("System:0->Cabinet:1003->Chassis:7->NodeBlade:7->NodeCard:1->Node:1")
+	if err != nil {
+		t.Fatalf("error parsing location1, err: %v", err)
+	}
+
+	v := CompareLocationPath(location0, location1)
+	if !v {
+		t.Fatalf("Expected true when comparing locations, location0: %v, location1: %v", location0, location1)
+	}
+
+	v = CompareLocationPath(location1, location0)
+	if v {
+		t.Fatalf("Expected true when comparing locations, location1: %v, location0: %v", location1, location0)
+	}
+
+	locationChassis, err := toLocationPath("System:0->Cabinet:1003->Chassis:7")
+	if err != nil {
+		t.Fatalf("error parsing locationChassis, err: %v", err)
+	}
+
+	v = CompareLocationPath(location1, locationChassis)
+	if !v {
+		t.Fatalf("Expected true when comparing locations, location1: %v, locationChassis: %v", location1, locationChassis)
+	}
+
+	v = CompareLocationPath(locationChassis, location1)
+	if v {
+		t.Fatalf("Expected false when comparing locations, locationChassis: %v, location1: %v", locationChassis, location1)
 	}
 }
